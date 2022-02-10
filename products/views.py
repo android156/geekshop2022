9d5ac7c255e35165d8c41eea167205bb64332cda
android156 <n_degtyarev@mail.ru>
2022-02-10 11:13:40 +0300

    context added

--- products/views.py
+++ products/views.py
@@ -1,11 +1,27 @@
+import json
+
 from django.shortcuts import render
 
 # Create your views here.
+from geekshop.settings import BASE_DIR
+
+
+def read_json_orders(filename):
+    with open(filename, 'r') as json_file:
+        json_data = json.load(json_file)
+    return json_data
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        "title": "Geekshop Главная",
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        "title": "Geekshop Продукты",
+        "products": read_json_orders(BASE_DIR / 'products/fixtures/products.json')
+    }
+    return render(request, 'products/products.html', context)
