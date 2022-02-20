9bbfa751f241f91b3c6b5cc866392cb692c07680
android156 <n_degtyarev@mail.ru>
2022-02-20 17:15:05 +0300

    form and logic for form added

--- users/views.py
+++ users/views.py
@@ -1,15 +1,27 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import UserLoginForm
 
 # Create your views here.
-from geekshop.settings import BASE_DIR
-from django.conf import settings
-
-from users.models import User
-
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
     context = {
         "title": "GeekShop - Авторизация",
+        "form": UserLoginForm,
     }
     return render(request, 'users/login.html', context)
 
