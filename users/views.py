0d00bb18449e4e21f0ee9e459f6b2fa6d69b4cde
android156 <n_degtyarev@mail.ru>
2022-02-24 15:44:18 +0300

    added messages and colors for errors and warnings

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -34,6 +34,7 @@ def register(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Регистрация прошла успешно!!!')
             return HttpResponseRedirect(reverse('users:login'))
         else:
             print(form.errors)
