03935319558487bc58a62276316f8c212ef08754
android156 <n_degtyarev@mail.ru>
2022-03-25 18:54:21 +0300

    email_verification template, verify controller added

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path
 
 from users.views import login, register, logout, profile
@@ -11,5 +10,6 @@ urlpatterns = [
     path('registration/', register, name='registration'),
     path('logout/', logout, name='logout'),
     path('profile/', profile, name='profile'),
+    path('verify/<str:email>/<str:activation_key>/', verify, name='verify'),
 
 ]
