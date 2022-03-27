1be3c1dc8e9a9cd3be3bae86fe04d7b017076db0
android156 <n_degtyarev@mail.ru>
2022-03-27 18:49:28 +0300

    http://127.0.0.1:8000/users/edit/ - works, but for editing profile we already have http://127.0.0.1:8000/users/profile/

--- users/models.py
+++ users/models.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.timezone import now
 from datetime import timedelta
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -24,3 +25,25 @@ class User(AbstractUser):
             return False
         else:
             return True
+
+
+class ShopUserProfile(models.Model):
+    FAT = 'F'
+    SLIM = 'S'
+    POWER_CHOICES = (
+        (FAT, 'Толстый'),
+        (SLIM, 'Тонкий'),
+    )
+    user = models.OneToOneField(User, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+    tagline = models.CharField(verbose_name='теги', max_length=128, blank=True)
+    aboutMe = models.TextField(verbose_name='о себе', max_length=512, blank=True)
+    powerness = models.CharField(verbose_name='мощь', max_length=1, choices=POWER_CHOICES, blank=True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
