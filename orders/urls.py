159c274113c8212d906c54fee6ce66bcfd8b39ac
android156 <n_degtyarev@mail.ru>
2022-04-07 11:34:16 +0300

    update, delete controllers and confirm delete template added

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from orders.views import OrderList, OrderItemsCreate
+from orders.views import OrderList, OrderItemsCreate, OrderItemsUpdate
 
 app_name = 'orders'
 
@@ -8,6 +8,10 @@ urlpatterns = [
 
     path('', OrderList.as_view(), name='index'),
     path('create/', OrderItemsCreate.as_view(), name='order_create'),
+    path('forming/complete/<int:pk>/', order_forming_complete, name='order_forming_complete'),
+    path('read/<int:pk>/', OrderRead.as_view(), name='order_read'),
+    path('update/<int:pk>/', OrderItemsUpdate.as_view(), name='order_update'),
+    path('delete/<int:pk>/', OrderDelete.as_view(), name='order_delete'),
 
 
 ]
